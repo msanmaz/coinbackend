4ca2a65cc5f93c52b103f15f58df4ddadad81bc1
msanmaz <osanmaz4992@gmail.com>
2022-05-03 19:40:49 +0100

    sockets

--- server.js
+++ server.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const  cron  = require('node-cron');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
+const http = require('http')
+const { Server } = require("socket.io");
+const cors = require('cors');
+const { info } = require('console');
 const port = 3000;
 const app = express();
 
-app.listen(port, () => {
+app.use(cors())
+
+const server = http.createServer(app)
+
+var interval = setInterval(updateData, 2000);
+
+
+
+const io = new Server(server, {
+    transports:['polling'],
+    cors: {
+        origin:'*',
+        methods: ["GET","POST"]
+    }
+})
+
+
+
+var cachedData;
+
+
+io.on('connection', function(socket){
+    console.log('user is connected '+socket.id)
+    io.sockets.emit('data',cachedData);
+
+})
+
+
+function updateData() {
+    fetch('https://api.coincap.io/v2/assets')
+      .then(response=>response.json())
+      .then(result => {
+        cachedData = result;
+        io.sockets.emit('data', result);
+    })
+  }
+  
+
+
+
+
+server.listen(port, () => {
     console.log(`test server listening at http://localhost:${port}`)
 })
+
 
 
 values = [];
@@ -19,7 +65,6 @@ cron.schedule("*/2 * * * * *", async function() {
         }
         values.push(item)
     })
-    console.log('every 2sec')
 });
 
 
